98e5de4c4491535072218713d1ed276317ef068b
Faisal135711 <alpha316mnr@gmail.com>
2024-05-04 16:57:29 +0600

    Add mongoose

--- app.js
+++ app.js
@@ -1,9 +1,39 @@
 var express = require('express');
+var mongoose = require("mongoose");
+
+var user = require("./user");
+
+require("dotenv").config()
+
+async function connectDB() {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("db connected successfully");
+    } catch(error) {
+        console.log(error)
+    }
+}
+
+connectDB();
 
 var app = express();
 
 app.use('/', (req, res) => res.send("Hello from Node API"));
+app.post("/user", async (req, res) => {
+    try {
+        const { name, email } = req.body;
 
-app.listen(5000, () => console.log("Server is running"));
+        const newUser = await user.create({ name, email });
+
+        return res.status(201).json({
+            message: "User created",
+            user: newUser
+        });
+    } catch (error) {
+        res.error(error)
+    }
+})
+
+app.listen(process.env.PORT, () => console.log("Server is running on ", process.env.PORT));
 
 module.exports = app;
---
+++ user.js
@@ -0,0 +1,25 @@
+// user.js
+
+const mongoose = require('mongoose');
+
+// Define the user schema
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true, // Ensure username is unique
+        trim: true // Trim whitespace from username
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true, // Ensure email is unique
+        trim: true, // Trim whitespace from email
+        lowercase: true, // Convert email to lowercase
+        match: [/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, 'Please enter a valid email address'] // Validate email format
+    }
+});
+
+// Create and export the User model based on the schema
+const user = mongoose.model('User', userSchema);
+module.exports = user;
