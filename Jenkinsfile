49cab9b9dede244687e52cb013375dacb4d45ff8
Faisal135711 <alpha316mnr@gmail.com>
2024-06-01 18:27:05 +0600

    Add Jenkinfile with declarative syntax

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,27 +1,21 @@
 pipeline {
-  agent any
+    agent any
 
-  stages {
-    stage("build") {
-      steps {
-        echo "building the application!!!"
-        nodejs('Node-22.2.0') {
-          sh 'npm install'
-          sh 'npm start'
+    stages {
+        stage('Build') {
+            steps {
+                echo 'Building...'
+            }
         }
-      }
+        stage('Test') {
+            steps {
+                echo 'Testing...'
+            }
+        }
+        stage('Deploying') {
+            steps {
+                echo 'Deploying...'
+            }
+        }
     }
-
-    stage("test") {
-      steps {
-        echo "testing the application..."
-      }
-    }
-
-    stage("deploy") {
-      steps {
-        echo "deploying the application..."
-      }
-    }
-  }
 }
--- app.js
+++ app.js
@@ -1,55 +1,12 @@
 var express = require("express");
-var mongoose = require("mongoose");
 var cors = require("cors");
-
-var user = require("./user");
-
-require("dotenv").config();
-
-async function connectDB() {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("db connected successfully");
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-// connectDB();
 
 var app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/api/", (req, res) => res.send("Hello from Node API"));
-
-app.get("/api/user", async (req, res) => {
-  try {
-    const users = await user.find();
-    return res.status(200).json({
-      users,
-    });
-  } catch (error) {
-    console.log(error);
-    res.send(error);
-  }
-});
-
-app.post("/api/user", async (req, res) => {
-  try {
-    const { name, email } = req.body;
-
-    const newUser = await user.create({ name, email });
-
-    return res.status(201).json({
-      message: "User created",
-      user: newUser,
-    });
-  } catch (error) {
-    res.send(error);
-  }
-});
+app.get("/api/", (req, res) => res.send("Hello Jenkins!!!"));
 
 const PORT = 3000;
 
