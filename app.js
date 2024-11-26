82763d61d2d865085a1a385b70c65651d2333b62
Faisal135711 <alpha316mnr@gmail.com>
2024-11-26 22:22:54 +0600

    Github Actions: Fix mongodb issue

--- app.js
+++ app.js
@@ -11,13 +11,8 @@ app.use(express.static(path.join(__dirname, "/")));
 app.use(cors());
 
 mongoose.connect(
-  process.env.MONGO_URI,
-  {
-    // user: process.env.MONGO_USERNAME,
-    // pass: process.env.MONGO_PASSWORD,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  },
+  `mongodb+srv://faisal:faisal@devcluster.vbvewfl.mongodb.net/?retryWrites=true&w=majority`,
+  { useNewUrlParser: true, useUnifiedTopology: true },
   function (err) {
     if (err) {
       console.log("error!! " + err);
