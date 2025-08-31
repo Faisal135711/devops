9bb3a995428ab6dd8d08d616210c5fc187b94217
Faisal135711 <alpha316mnr@gmail.com>
2025-08-31 10:00:36 +0600

    Add service container

--- data/database.js
+++ data/database.js
@@ -5,14 +5,14 @@ const dbUser = process.env.MONGODB_USERNAME;
 const dbPassword = process.env.MONGODB_PASSWORD;
 const dbName = process.env.MONGODB_DB_NAME;
 
-const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority&authSource=admin`;
+const uri = `mongodb://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority&authSource=admin`;
 const client = new MongoClient(uri);
 
 console.log("Trying to connect to db");
 
 try {
-  // await client.connect();
-  // await client.db(dbName).command({ ping: 1 });
+  await client.connect();
+  await client.db(dbName).command({ ping: 1 });
   console.log("Connected successfully to server");
 } catch (error) {
   console.log("Connection failed.", error);
--- routes/events.js
+++ routes/events.js
@@ -1,20 +1,20 @@
 import { Router } from "express";
 
-// import db from '../data/database.js';
+import db from "../data/database.js";
 
 const router = Router();
 
 router.get("/", async (req, res) => {
-  // const allEvents = await db.collection("events").find().toArray();
-  res.json({ events: [] });
+  const allEvents = await db.collection("events").find().toArray();
+  res.json({ events: allEvents });
 });
 
 router.post("/", async (req, res) => {
   const eventData = req.body;
-  // const result = await db.collection("events").insertOne({ ...eventData });
+  const result = await db.collection("events").insertOne({ ...eventData });
   res.status(201).json({
     message: "Event created.",
-    event: { ...eventData, id: 1 },
+    event: { ...eventData, id: result.insertedId },
   });
 });
 
