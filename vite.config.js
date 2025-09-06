7e579dfcf5fb77f092f2a56c84c627ce6d829b05
Faisal135711 <alpha316mnr@gmail.com>
2025-09-06 22:37:49 +0600

    Fix issues

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/devops/", // must match your repo name
-  build: {
-    outDir: "dist",
-    assetsDir: "assets", // keep js/css in assets/
-  },
   test: {
     globals: true,
     environment: "jsdom",
